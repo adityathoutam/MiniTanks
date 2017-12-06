6de075abc5ba4cdc2fe542de135ba4a783b144cf
Aditya Thoutam <adityathoutam@outlook.com>
2017-12-06 09:42:36 +0530

    update

---
+++ Assets/Scripts/Camera/Focus.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Focus : MonoBehaviour {
+
+    public Transform Map;
+    public Transform L1;
+    public Transform L2;
+    public Transform L3;
+    public Transform L4;
+    public Transform L5;
+    Vector3 offset;
+   
+
+    void Start () {
+
+       //offset = Map.position - transform.position;
+	}
+
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            StartCoroutine(Focusing(transform.position,L2.transform.position));
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            StartCoroutine(Focusing(transform.position, L3.transform.position));
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            StartCoroutine(Focusing(transform.position, L4.transform.position));
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            StartCoroutine(Focusing(transform.position, L5.transform.position));
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            StartCoroutine(Focusing(transform.position, L1.transform.position));
+        }
+    }
+
+
+    IEnumerator Focusing(Vector3 start,Vector3 target)
+    {
+        float t = 0;
+        while(t<=1.5)
+        {
+            t += Time.deltaTime * (Time.timeScale / 0.5f);
+            Vector3 temp = Vector3.Lerp(start, target, t);
+            transform.position = new Vector3(temp.x, temp.y, transform.position.z);
+            yield return null;
+            
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Defense/AIScript.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AIScript : MonoBehaviour
+{   //storing triangles and circles
+    private List<GameObject> _triangles = new List<GameObject>();
+    private List<GameObject> _circles   = new List<GameObject>();
+
+    private List<GameObject> _blueMap   = new List<GameObject>();
+    private List<GameObject> _greenMap  = new List<GameObject>();
+    private List<GameObject> _brownMap  = new List<GameObject>();
+    private List<GameObject> _orangeMap = new List<GameObject>();
+    private List<GameObject> _pinkMap   = new List<GameObject>();
+
+    public GameObject TriPrefab;
+    public GameObject CirclePrefab;
+
+    public Transform point1;
+    public Transform point2;
+    public Transform point3;
+   
+
+#region CreatingInstances
+    private void CreateTriangleInstances(int instanceCount)
+    {
+            for (int i = 0; i < instanceCount; ++i)
+        {
+            var instance = Instantiate(TriPrefab) as GameObject;
+            _triangles.Add(instance);
+        }
+    }
+    private void CreateCircleInstances(int instanceCount)
+    {
+        for (int i = 0; i < instanceCount; ++i)
+        {
+            var instance = Instantiate(CirclePrefab) as GameObject;
+            _circles.Add(instance);
+        }
+    }
+#endregion
+    private void Start()
+    {
+        
+            CreateTriangleInstances(1);
+            CreateCircleInstances(1);
+    }
+
+    private void Update()
+    {
+
+        CircleMove(_circles[0], point1, point2, point3);
+       // TriangleMove(_triangles[0], point1, point2);
+
+    }
+#region MovingEnemies
+    void CircleMove(GameObject CircleInstance, Transform firstTriPoint, Transform SecondTriPoint, Transform ThirdTriPoint)
+    {
+
+        int speed = 15;
+        Vector3 firstpoint = firstTriPoint.position;
+        Vector3 secondpoint = SecondTriPoint.position;
+        Vector3 secpoint =   SecondTriPoint.position;
+        Vector3 thirdpoint = ThirdTriPoint.position;
+
+
+        Vector3 directionVector = (secondpoint - CircleInstance.transform.position);
+        float angle = Mathf.Atan2(directionVector.x, directionVector.y) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+        CircleInstance.transform.rotation = Quaternion.Lerp(CircleInstance.transform.rotation, rotation, 10 * Time.deltaTime);
+        CircleInstance.transform.Translate(Vector3.up * speed * Time.deltaTime, Space.Self);
+
+
+        if (Vector3.Distance(CircleInstance.transform.position, secondpoint) < 0.2f)
+        {
+            int random = Random.Range(0, 2);
+            if (random == 0)
+            {
+                secondpoint = thirdpoint;
+            }
+            if (random == 1)
+            {
+                secondpoint = firstpoint;
+            }
+        }
+
+        if (Vector3.Distance(CircleInstance.transform.position, thirdpoint) < 0.2f)
+        {
+            int random = Random.Range(0, 2);
+            if (random == 0)
+            {
+                secondpoint = firstpoint;
+            }
+            if (random == 1)
+            {
+                secondpoint = secpoint;
+
+            }
+        }
+
+        if (Vector3.Distance(CircleInstance.transform.position, firstpoint) < 0.2f)
+        {
+            int random = Random.Range(0, 2);
+            if (random == 0)
+            {
+                secondpoint = secpoint;
+
+            }
+            if (random == 1)
+            {
+                secondpoint = thirdpoint;
+            }
+        }
+
+    }
+
+    void TriangleMove(GameObject triangleinstance, Transform firstpoint, Transform secondpoint)
+    {
+        float time = 0f;
+        float speed = 15f;
+        Vector3 spawnPoint = firstpoint.transform.position;
+        Vector3 uppoint = firstpoint.position;
+        Vector3 downpoint = secondpoint.position;
+        Vector3 target = downpoint;
+
+        Vector3 directionVector = (downpoint - triangleinstance.transform.position);
+        float angle = Mathf.Atan2(directionVector.x, directionVector.y) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+        time += 0.01f;
+        if (time > 0.5f)
+            time = 0;
+        triangleinstance.transform.rotation = Quaternion.Lerp(triangleinstance.transform.rotation, rotation, time);
+        triangleinstance.transform.Translate(Vector3.up * speed * Time.deltaTime, Space.Self);
+
+
+        if (Vector3.Distance(triangleinstance.transform.position, downpoint) < 0.2f)
+        {
+            downpoint = uppoint;
+        }
+
+        if (Vector3.Distance(triangleinstance.transform.position, uppoint) < 0.2f)
+        {
+            downpoint = target;
+        }
+
+    }
+#endregion
+}
