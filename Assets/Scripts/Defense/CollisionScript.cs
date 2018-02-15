5eae271e0746772b0eddd8e9863859e0a46045ac
Aditya Thoutam <adityathoutam@outlook.com>
2018-02-15 19:51:49 +0530

    Revert "Revert "Revert "Revert "level2 works""""
    This reverts commit 89b79bd02700a08f6b758cf71963efdec7d0b04d.

---
+++ Assets/Scripts/Defense/CollisionScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour {
+
+
+    
+    public GameObject Player;
+
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+
+    void Start () {
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("gg");
+
+            Enemy1.SetActive(false);
+            Enemy2.SetActive(false);
+
+        }
+    }
+
+    void Update () {
+		
+	}
+}
